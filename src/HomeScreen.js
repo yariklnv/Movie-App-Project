530a2afbbd5bbbb516f1a9a5b4441ff4eb1177b9
Souleiman <nshutisouleiman@gmail.com>
2025-04-01 19:58:42 +0200

    commit 2

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -3,6 +3,7 @@
  import './HomeScreen.css';
  import Nav from './Nav.js';
  import requests from './Requests';
+ import Row from './Row';
 
 
  function HomeScreen() {
@@ -13,7 +14,18 @@
         
         <Banner/>
 
-        {/* Row */}
+        <Row title='NETFLIX ORIGINALS' 
+             fetchUrl={requests.fetchNetflixOriginals} 
+             isLargeRow 
+        />
+        <Row title='Top Rated' fetchUrl={requests.fetchTopRated} />
+        <Row title='Action Movies' fetchUrl={requests.fetchActionMovies} />
+        <Row title='Comedy Movies' fetchUrl={requests.fetchComedyMovies} />
+        <Row title='Horror Movies' fetchUrl={requests.fetchHorrorMovies} />
+        <Row title='Romance Movies' fetchUrl={requests.fetchRomanceMovies} />
+        <Row title='NETFLIX ORIGINALS' fetchUrl={requests.fetchNetflixOriginals} />
+        <Row title='Documentaries' fetchUrl={requests.fetchDocumentaries} />
+
      </div>
    )
  }
