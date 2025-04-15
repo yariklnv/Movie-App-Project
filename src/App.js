5c72c92506aaf5d243feb04230f6a5d103d525d8
Selmy44 <nshutisouleiman@gmail.com>
2025-04-15 22:48:05 +0200

    commit 11

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import HomeScreen from './screens/HomeScreen';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
@@ -7,6 +7,16 @@ import LoginScreen from './screens/LoginScreen';
 
 function App() {
   const user = null;
+
+useEffect(() => {
+  auth.onAuthStateChanged((authUser) => {
+    if (authUser) {
+      console.log('USER IS LOGGED IN', authUser);
+    } else {
+      console.log('USER IS NOT LOGGED IN');
+    }
+  });
+}, []);
 
   return (
     <div className="app">
